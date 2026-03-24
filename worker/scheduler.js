fe340d3d8cff532806acd4d67695e09be8453af2
Satvik <satvik2006sharma@gmail.com>
2026-03-25 00:03:33 +0530

    feat(worker): enhance monitor processing with logging and status updates

---
+++ worker/scheduler.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+import connectDB from "../backend/src/db/index.js"
+import { Monitor } from "../backend/src/models/monitors.models.js"
+import monitorQueue from "./queue.js"
+
+
+await connectDB()
+
+async function startScheduler() {
+    console.log("Scheduler starting...")
+
+    const monitors = await Monitor.find()
+
+    for (const monitor of monitors) {
+        console.log("Scheduling monitor:", monitor._id)
+
+        await monitorQueue.add("monitor-api",
+            { monitorId: monitor._id.toString() },
+            {
+                jobId: monitor._id.toString(),
+                repeat: {
+                    every: 10000
+                }
+            }
+        )
+    }
+    console.log("All monitors scheduled");
+}
+
+startScheduler()
--- worker/worker.js
+++ worker/worker.js
@@ -1,7 +1,8 @@
 import { Worker } from "bullmq";
 import axios from "axios";
 import connectDB from "../backend/src/db/index.js"
-import {Monitor} from "../backend/src/models/monitors.models.js"
+import { Monitor } from "../backend/src/models/monitors.models.js"
+import { MonitorLog } from "../backend/src/models/monitorLog.models.js"
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -29,12 +30,25 @@ const worker = new Worker("monitorQueue",
             console.log("UP:", monitor.url);
             console.log("Response time:", responseTime, "ms");
 
-         
-            console.log("Saving log:", {
-                monitorId,
+            const newStatus = "UP";
+
+
+            await MonitorLog.create({
+                monitorId: monitorId,
                 status: "UP",
-                responseTime,
-            });
+                checkedAt: new Date(start),
+                responseTime: responseTime,
+            })
+
+            if (monitor.lastStatus !== newStatus) {
+                console.log(`Status changed: ${monitor.lastStatus} -> ${newStatus}`)
+            }
+
+            await Monitor.findByIdAndUpdate(monitorId, {
+                lastStatus: newStatus,
+                lastCheckedAt: new Date(),
+            })
+
 
         } catch (err) {
             const responseTime = Date.now() - start;
@@ -42,12 +56,15 @@ const worker = new Worker("monitorQueue",
             console.log("DOWN:", monitor.url);
             console.log("Response time:", responseTime, "ms");
 
-           
-            console.log("Saving log:", {
-                monitorId,
+
+
+            await MonitorLog.create({
+                monitorId: monitorId,
                 status: "DOWN",
-                responseTime,
-            });
+                checkedAt: new Date(start),
+                responseTime: responseTime,
+                errorMessage: err.message
+            })
         }
     },
     {
